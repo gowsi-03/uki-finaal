26ca88b0db9e766938dd0a05455a090bda8dc4dd
gowsi-03 <gowsip2@gmail.com>
2022-09-12 09:30:58 +0530

    First commit

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import App from './App'
+
+import "remixicon/fonts/remixicon.css";
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './bootstrap.min.css'
+import './index.css'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
